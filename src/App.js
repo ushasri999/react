1651a06220b9af5eb27756e08cda646fb97427a1
ushasri999 <modhugumudiushasri@gmail.com>
2023-12-08 07:17:02 +0530

    add form handling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { useState } from 'react'
 
 function App() {
-  const [name, setName] = useState('');
+  const [data, setData] = useState({
+    email:'',
+    password:''
+  })
+
+  const changeHandler = e => {
+    setData({...data, [e.target.name] : e.target.value})
+  }
+  const submitHandler = e => {
+    e.preventDefault();
+    console.log(data);
+  }
+  
   return (
     <div>
       <center>
-        Name : {name} <br></br>
-        <input type='text' name='name'onChange={(e) => setName(e.target.value)}/>
+        <form onSubmit={submitHandler}>
+          <label>E-Mail</label><br/>
+          <input type = 'text' name = 'email' onChange={changeHandler}/><br/>
+          
+          <label>Password</label><br/>
+          <input type = 'password' name = 'password' onChange={changeHandler}/><br/>
+          <input type='submit' value='Login'/>
+        </form>
       </center>
-    </div>
+    </div> 
   )
 }
 
