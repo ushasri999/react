87354a1949b5a2fa7cbe1e78a6c456fe16e9d13d
MODHUGUMUDI USHA SRI <modhugumudiushasri@gmail.com>
2023-12-17 19:14:50 +0530

    navigation between pages using Link

--- src/About.js
+++ src/About.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <center>
         <div>
         <h3>Welcome to About page</h3>
+        <Link to = '/'>Back to home</Link>
         </div>
     </center>
   )
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <center>
         <div>
         <h3>Welcome to home page</h3>
+        <Link to = '/dashboard'>Dashboard</Link> {" "}
+        <Link to = '/about'>About</Link>
         </div>
     </center>
   )
