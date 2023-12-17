767aad06a20f6ffd8259d702278382afdfb455ed
MODHUGUMUDI USHA SRI <modhugumudiushasri@gmail.com>
2023-12-17 08:25:15 +0530

    submit botton with bootstramp

--- src/App.js
+++ src/App.js
@@ -11,23 +11,29 @@ function App() {
   }
   const submitHandler = e => {
     e.preventDefault();
-    console.log(data);
+    
+    if(data.password.length() < 5){
+      alert('provide password of length > 5')
+    }
+    else{
+      console.log(data);
+    }
   }
   
   return (
-    <div>
-      <center>
-        <form onSubmit={submitHandler}>
-          <label>E-Mail</label><br/>
-          <input type = 'text' name = 'email' onChange={changeHandler}/><br/>
-          
-          <label>Password</label><br/>
-          <input type = 'password' name = 'password' onChange={changeHandler}/><br/>
-          <input type='submit' value='Login'/>
-        </form>
-      </center>
-    </div> 
+    <center>
+      <div>
+          <form onSubmit={submitHandler}>
+            <label>E-Mail</label><br/>
+            <input type = 'text' name = 'email' onChange={changeHandler}/><br/>
+            
+            <label>Password</label><br/>
+            <input type = 'password' name = 'password' onChange={changeHandler}/><br/>
+            <input type='submit' value='Login' className='btn btn-primary'/>
+          </form>
+      </div> 
+    </center>
   )
 }
 
-export default App
+export default App;
