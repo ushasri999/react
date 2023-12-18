244d62fa95714c32ad279a0612e45a5c271920c4
MODHUGUMUDI USHA SRI <modhugumudiushasri@gmail.com>
2023-12-18 14:22:52 +0530

    use pathparams

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 const Home = () => {
+  let params = useParams();
   return (
     <center>
         <div>
         <h3>Welcome to Dashboard page</h3>
+        <p>Name: {params.name}</p>
         <Link to = '/' className = 'Link'>Back to home</Link>
         </div>
     </center>
